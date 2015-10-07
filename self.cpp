643eea9ad9aea30d9062c16266bf9e9c80564aa4
gonglong <mr.gonglong@outlook.com>
2015-10-06 23:33:47 -0400

    second commit

---
+++ self.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string a = "#include <iostream>#include <string>using namespace std;int main(){string a = ;cout << a.substr(0,78)<< char(34) << a << char(34) << a.substr(78) << endl;return 0;}";
+	
+	
+	
+	cout << a.substr(0,78)<< char(34) << a << char(34) << a.substr(78) << endl;
+	
+	
+	return 0;
+}
